2b0d1f6eba5b20e243bd5be6d77bab917c5d7e67
lxy <gmwzxiaotaiyang@gmail.com>
2024-09-14 17:32:22 +0800

    login page

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import index from '@/views/index.vue'
 import test from '@/views/test/index.vue'
+import login from '@/views/login.vue'
 
 const routes = [
   { path: '/', component: index },
   { path: '/test', component: test },
+  { path: '/login', component: login },
 ]
 
 const router = createRouter({
---
+++ src/views/login.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="login-frame">
+    <div class="system-logo">
+      <div>
+        扬州大自然
+      </div>
+    </div>
+    <div class="login-input">
+      <div class="input-body">
+        <h4>欢迎使用 ******* 系统！</h4>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.login-frame {
+  display: grid;
+  grid-template-columns: minmax(250px, 50%) 1fr;
+  min-width: 800px;
+  min-height: 500px;
+
+  // background-color: rgb(206 206 206);
+  border-radius: 20px;
+  box-shadow: 5px 5px 10px rgb(0 0 0 / 30%);
+}
+
+.system-logo {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: rgb(238 238 238);
+  border-radius: 20px 0 0 20px;
+}
+
+.login-input {
+  width: 100%;
+  height: 100%;
+  margin: auto;
+
+  .input-body {
+    margin: 20px;
+  }
+}
+</style>
